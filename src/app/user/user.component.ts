a397f17951b4fb20a2b7ced1a82b6fe88a25f921
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-08-01 23:33:51 +0200

    ng model function for dialog

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { User } from 'src/models/user.class';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
@@ -9,7 +10,13 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 })
 export class UserComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+  user = new User();
+ 
+
+
+  constructor(public dialog: MatDialog) { 
+    
+  }
 
   ngOnInit(): void {
   }
